03d6330e237e73dfb1726ffa76b22acee061fbb3
Elias.Lunden.TIVI <53103@edu.raseko.fi>
2024-10-02 23:49:09 +0300

    V1.1B - [2/10/24]

--- Roulette.py
+++ Roulette.py
@@ -1,4 +1,4 @@
-# REVOLV6 - .PY CMD CODE - V1B - [2/10/24]
+# REVOLV6 - .PY CMD CODE - V1.1B - [2/10/24]
 # E:L - N3xu_Ss
 
 
@@ -7,7 +7,6 @@
 
 import random
 
-8
 # Prewarmed Values
 
 
@@ -15,18 +14,10 @@ Round = 0
 Money = 0
 Multiplier = 0.5 * Round
 GameOn = True
-
 Chambers = [0, 0, 0, 0, 0, 1]
 
 
-
-#
 print("***CLACK*** the revolvers cylinder swings open. 5 empty chambers. One live...")
-#
-#
-
-
-
 
 
 print("""
@@ -44,6 +35,7 @@ Roll - Roll the cylinder, reset the state of the revolver... Just hope you dont 
 while GameOn == True:
     #
     print("Round " + str(Round) + " ...") 
+    print("You have " + str(Money) + " Money on the line... Or to spend... ")
     #Displays current round number. Starts at given value. [DEF = 0]
     choice = input("Choose your fate: ").capitalize()
     #If player decides to Shoot
@@ -54,7 +46,8 @@ while GameOn == True:
         if Chamber == 0:
             print("***CLICK*** Empty... Just like your head if that were a live round... Live to see another round.")
             Round = Round + 1
-            Money = Money + 25
+            Multiplier = 0.5 * Round
+            Money = Money + 25 * Multiplier
             Chambers.remove(0)
         #Code if the requirement for a live round is met. Ends the game. [DEF = 1]
         if Chamber == 1:
@@ -64,10 +57,11 @@ while GameOn == True:
     #Code if the player re-rolls the cylinder
     if choice == "Roll":
         #Checks if player has enough money to re-roll. [DEF Needed = 50]
-        if Money >= 50: 
+        if Money >= 10 * Multiplier: 
             print("The revolver is grabbed by a person behind you... He opens it with a clack and spins the cylinder... Reseting the chances...")
             #Resets the chambers and the chances.
             Chambers = [0, 0, 0, 0, 0, 1] 
+            Money = Money - 10 * Multiplier
         #Message if the player lacks the sufficient money to reroll... Resets the current round to the start.
         else: print("- Hey dipshit... you dont have enough money to reroll. A man laughs behind you. Seemingly the big boss of this place...")
     
@@ -83,7 +77,7 @@ if Dead == False:
     print("Game statistics: Money earned - " + str(Money) + " Rounds survived " + str(Round))
 #Ending message after lobotomy via 44 magnum.
 if Dead == True:
-    print("Game statistics: Money - " + str(Money) + " Rounds survived before death " + str(Round))
+    print("Game statistics: Money: " + str(Money) + " Rounds survived before death: " + str(Round))
 
 
 
