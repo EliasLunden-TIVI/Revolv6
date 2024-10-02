6515b5b3e13b90b3b1718c6543392be236b6840a
Elias.Lunden.TIVI <53103@edu.raseko.fi>
2024-10-02 16:16:32 +0300

    Preplace

---
+++ Roulette.py
@@ -0,0 +1,20 @@
+# REVOLV6 - .PY CMD CODE
+
+
+
+# INFO LISTED ON WIKIPAGE
+
+import random
+
+8
+# Prewarmed Values
+
+
+Round = 1
+Money = 0
+
+
+Chambers = [0, 0, 0, 0, 0, 1]
+
+
+print("Round" + Round + "...")
